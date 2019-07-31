b6c1cb4dbbbbc68058216604515c504b2451a7c7
youngeon.kim <young.kim@medipixel.io>
2019-08-01 08:46:56 +0900

    added set_leader, show_names

--- structural/composite.py
+++ structural/composite.py
@@ -9,27 +9,42 @@ class RollCall(ABC):
 
 
 class Soldier(RollCall):
+    def __init__(self, name="Joe"):
+        self.name = name
+
     def get_num_member(self):
         return 1
 
+    def show_names(self):
+        print(self.name)
+
 
 class Squad(RollCall):
-    def __init__(self):
-        self.squad_leader = Soldier()
+    def __init__(self, leader_name="Smith"):
+        self.set_leader(Soldier(leader_name))
         self.parts = []
+
+    def set_leader(self, soldier):
+        self.squad_leader = soldier
 
     def get_num_member(self):
         return sum([x.get_num_member() for x in self.parts]) + self.squad_leader.get_num_member()
 
-    def add_member(self):
-        self.parts.append(Soldier())
+    def show_names(self):
+        print(self.squad_leader.name)
+        for x in self.parts:
+            x.show_names()
+
+    def add_member(self, name):
+        self.parts.append(Soldier(name))
 
 
 squad = Squad()
 print(squad.get_num_member())
-squad.add_member()
-squad.add_member()
-squad.add_member()
-squad.add_member()
-squad.add_member()
+squad.add_member("Prince")
+squad.add_member("Fire")
+squad.add_member("Faker")
+squad.add_member("Flash")
+squad.add_member("Mesh")
 print(squad.get_num_member())
+squad.show_names()
