91e6d855e96ca225d5ab3536b6df688449beb475
youngeon.kim <young.kim@medipixel.io>
2019-08-08 09:26:56 +0900

    temporary saving 2

--- creational/factory.py
+++ creational/factory.py
@@ -11,11 +11,33 @@ class ControlerFactory(ABC):
         pass
 
 
-
-class WirelessControler(ABC):
+class AbstractControler(ABC):
 
     def __init__(self, channel):
         self.channel = channel
 
     @abstractmethod
-    def 
+    def control(self):
+        pass
+
+
+class WirelessControlerFactory(ControlerFactory):
+
+    def __init__(self, creator_id):
+        super().__init__(creator_id)
+
+    def create_object(self, channel):
+        return WirelessControler(channel)
+
+
+class WireControlerFactory(ControlerFactory):
+
+    def __init__(self, creator_id):
+        super().__init__(creator_id)
+
+    def create_object(self, channel):
+        return WireControler(channel)
+
+class WirelessControler(AbstractControler):
+
+class WireControler(AbstractControler):
