f9bac261ccbed11755f834a13f4333c2aa3c008e
youngeon.kim <young.kim@medipixel.io>
2019-08-22 09:10:14 +0900

    Modify overal structure

--- creational/factory.py
+++ creational/factory.py
@@ -1,16 +1,24 @@
 from abc import ABC, abstractmethod
 
 
-class ControllerFactory:
+class ControllerFactory(ABC):
 
-    def __init__(self, creator_id):
-        self.id = creator_id
+    @abstractmethod
+    def create_object(self):
+        pass
 
-    def create_object(self, channel=None):
-        if channel is not None:
-            return WirelessController(channel)
-        else:
-            return WireController()
+
+class WirelessControllerFactory(ControllerFactory):
+
+    def create_object(self, channel):
+        return WirelessController(channel)
+
+
+
+class WireControllerFactory(ControllerFactory):
+
+    def create_object(self):
+        return WireController()
 
 
 class AbstractController(ABC):
@@ -33,15 +41,19 @@ class WirelessController(AbstractController):
 class WireController(AbstractController):
 
     def __init__(self):
-        print(f"This is wire controller. Channel number is not nedded")
+        print(f"This is wire controller.")
 
     def control(self):
-        print("I can control objects that has an outlet")
+        print("I can control objects that has an electric port")
 
 
 if __name__ == "__main__":
-    print("I need wireless controller")
-    controller_a = ControllerFactory('abc').create_object(10)
 
-    print("I need wire controller")
-    controller_b = ControllerFactory('efg').create_object()
+    wireless_controller_factory = WirelessControllerFactory()
+    wire_controller_factory = WireControllerFactory()
+
+    print("I need a remote controller for air conditioner")
+    air_conditioner_controller = wireless_controller_factory.create_object(10)
+
+    print("I need a part of fan controller")
+    fan_controller_part = wire_controller_factory.create_object()
