685ea615001df3c128ccbd14f15f2e7b6b212907
youngeon.kim <young.kim@medipixel.io>
2019-09-03 10:03:31 +0900

    fix spelling

--- creational/abstract_factory.py
+++ creational/abstract_factory.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class AbstractPizzaStore(ABC):
 
     @abstractmethod
-    def make_simple_pizza(self):
+    def make_original_pizza(self):
         pass
 
     @abstractmethod
@@ -18,81 +18,81 @@ class AbstractPizzaStore(ABC):
 
 class DominoPizza(AbstractPizzaStore):
 
-    topings = ['cheeze', 'pepperoni', 'pimento',
+    toppings = ['cheese', 'pepperoni', 'pimento',
                'potato', 'beef', 'bacon', 'shrimp',
                'lobster', 'caviar']
 
-    def make_simple_pizza(self):
-        return SimplePizza('just domino pizza', 10000, self.topings[:3])
+    def make_original_pizza(self):
+        return OriginalPizza('just domino pizza', 10000, self.toppings[:3])
 
     def make_premium_pizza(self):
-        return PremiumPizza('good domino pizza', 20000, self.topings[:7])
+        return PremiumPizza('good domino pizza', 20000, self.toppings[:7])
 
     def make_special_pizza(self):
-        return SpecialPizza('nice domino pizza', 100000, self.topings)
+        return SpecialPizza('nice domino pizza', 100000, self.toppings)
 
 
 class PizzaHut(AbstractPizzaStore):
 
-    topings = ['cheeze', 'pepperoni', 'potato',
+    toppings = ['cheese', 'pepperoni', 'potato',
                'pimento', 'beef', 'bacon', 'shrimp',
                'lobster']
 
-    def make_simple_pizza(self):
-        return SimplePizza('Hut happy', 15000, self.topings[:4])
+    def make_original_pizza(self):
+        return OriginalPizza('Hut happy', 15000, self.toppings[:4])
 
     def make_premium_pizza(self):
-        return PremiumPizza('Hut premium', 30000, self.topings[:6])
+        return PremiumPizza('Hut premium', 30000, self.toppings[:6])
 
     def make_special_pizza(self):
-        return SpecialPizza('Hut special', 50000, self.topings)
+        return SpecialPizza('Hut special', 50000, self.toppings)
 
 
-class SimplePizza:
+class OriginalPizza:
 
-    def __init__(self, name, price, topings):
+    def __init__(self, name, price, toppings):
         self.name = name
         self.price = price
-        self.topings = topings
+        self.toppings = toppings
 
     def describe(self):
-        print(f"{self.name} contains {self.topings}. It`s price is {self.price}")
+        print(f"{self.name} contains {self.toppings}. It`s price is {self.price}")
 
 
 class PremiumPizza:
 
-    def __init__(self, name, price, topings):
+    def __init__(self, name, price, toppings):
         self.name = name
         self.price = price
-        self.topings = topings
+        self.toppings = toppings
 
     def describe(self):
-        print(f"{self.name} contains {self.topings}. It`s price is {self.price}")
+        print(f"{self.name} contains {self.toppings}. It`s price is {self.price}")
 
 
 class SpecialPizza:
 
-    def __init__(self, name, price, topings):
+    def __init__(self, name, price, toppings):
         self.name = name
         self.price = price
-        self.topings = topings
+        self.toppings = toppings
 
     def describe(self):
-        print(f"{self.name} contains {self.topings}. It`s price is {self.price}. Too expensive")
+        print(f"{self.name} contains {self.toppings}. It`s price is {self.price}. Too expensive")
 
 
 def main():
     domino = DominoPizza()
     hut = PizzaHut()
 
-    domino_simple = domino.make_simple_pizza()
+    domino_simple = domino.make_original_pizza()
     domino_simple.describe()
     domino_premium = domino.make_premium_pizza()
     domino_premium.describe()
     domino_special = domino.make_special_pizza()
     domino_special.describe()
 
-    hut_simple = hut.make_simple_pizza()
+    hut_simple = hut.make_original_pizza()
     hut_simple.describe()
     hut_premium = hut.make_premium_pizza()
     hut_premium.describe()
